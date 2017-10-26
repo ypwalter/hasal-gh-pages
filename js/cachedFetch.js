679693b20e907f8d705d513dfc35957da0013108
Walter Chen <ypwalter@gmail.com>
2017-10-26 11:22:35 +0800

    adding new format json and archived old host.html

--- js/cachedFetch.js
+++ js/cachedFetch.js
@@ -1,11 +1,13 @@
+var filename = 'cachedFetch.js';
+
 // Need to import localforage:
 // https://cdnjs.cloudflare.com/ajax/libs/localforage/1.4.0/localforage.min.js
-function cachedFetch(url, expireTime) {
-  console.log("cachedFetch: Fetching " + url)
+function cachedFetch(url, expireTime = 3600000) {
+  console.log(filename + ": Checking " + url);
   return localforage.getItem(url).then(function(value){
     // create new cache if expired or empty (time is in ms)
     if (value == null || Date.now() - value.updated > expireTime){
-      console.log("cachedFetch: Redo fetching of data")
+      console.log(filename + ": Redo fetching of data");
       return fetch(url)
         .then(function(result){
           return result.json();
@@ -13,15 +15,26 @@ function cachedFetch(url, expireTime) {
         .then(function(value){
           return localforage.setItem(url, {'updated': Date.now(), 'data': value})
             .then(function(entry){
-              return entry['data']
+              return entry['data'];
             })
         })
     } else {
-      console.log("cachedFetch: Getting it directly from the cache")
+      console.log(filename + ": Getting it directly from the cache")
       return localforage.getItem(url)
         .then(function(entry){
-          return entry['data']
+          return entry['data'];
         })
     }
   })
 }
+
+async function blocking_cachedFetch(url, expireTime = 3600000) {
+  var obj = await cachedFetch(url, expireTime);
+  return obj;
+}
+
+function sleep (time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+

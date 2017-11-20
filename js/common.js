4ceef4f6a04abb9e608d1767b49d54b836cfea55
Walter Chen <ypwalter@gmail.com>
2017-11-20 18:06:46 +0800

    started to use real data in index.html

--- js/common.js
+++ js/common.js
@@ -1,6 +1,9 @@
 // get current html name
 var path = window.location.pathname;
 var page = path.split("/").pop();
+if(page == "") {
+    page = "index.html";
+}
 
 // get agent name
 var agent_name = get_request("agent");
